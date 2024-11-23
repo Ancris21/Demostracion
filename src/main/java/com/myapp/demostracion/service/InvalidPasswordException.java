c688905c5e1b9f2546918adbb6e88355befa91f0
Ancris21 <60703695+Ancris21@users.noreply.github.com>
2024-11-22 23:18:17 -0300

    Initial version of demostracion generated by generator-jhipster@8.7.3

---
+++ src/main/java/com/myapp/demostracion/service/InvalidPasswordException.java
@@ -0,0 +1,10 @@
+package com.myapp.demostracion.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+}
